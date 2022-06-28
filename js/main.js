7409180325e20a3fe39645dfe8e3eb2e147b7a5f
Vadim <65122882+Vadeem0550@users.noreply.github.com>
2022-06-28 13:56:11 +0300

    create_comments_data_2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-// //Функция, возвращающая случайное целое число
-// const getRandoms = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-// getRandoms(10, 500);
-//
+
 // //Функция для проверки максимальной длины строки
 // const getStringLength = (str, maxLength) => str.length <= maxLength;
 // getStringLength('Some very long text', 40);
@@ -14,15 +11,15 @@ const DESCRIPTIONS = ['Лето_2014', 'Школьные годы', 'Море_20
 
 const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-const likeValues = {
+const likeValue = {
   MIN: 15, MAX: 200
 };
 
-const CommentsValues = {
+const CommentsValue = {
   MIN: 1, MAX: 5
 };
 
-const AvatarValues = {
+const AvatarValue = {
   MIN: 1, MAX: 6
 };
 
@@ -42,17 +39,17 @@ const getCommentId = () => {
 
 const createRandomComments = () => ({
   id: getCommentId(),
-  avatar: `img/avatar-${getRandomValues(AvatarValues.MIN, AvatarValues.MAX)}.svg`,
+  avatar: `img/avatar-${getRandomValues(AvatarValue.MIN, AvatarValue.MAX)}.svg`,
   message: getRandomArrEl(MESSAGES),
   name: getRandomArrEl(NAMES),
 });
 
-const createPhotosRandom = () => Array.from({length: PHOTO_AMOUNT}, (item, index) => ({
+const createRandomPhotos = () => Array.from({length: PHOTO_AMOUNT}, (item, index) => ({
   id: index + 1,
   url: `photos/${index + 1}.jpg`,
   description: getRandomArrEl(DESCRIPTIONS),
-  likes: getRandomValues(likeValues.MIN, likeValues.MAX),
-  comments: Array.from({length: getRandomValues(CommentsValues.MIN, CommentsValues.MAX)}, createRandomComments)
+  likes: getRandomValues(likeValue.MIN, likeValue.MAX),
+  comments: Array.from({length: getRandomValues(CommentsValue.MIN, CommentsValue.MAX)}, createRandomComments)
 }));
-createPhotosRandom();
+createRandomPhotos();
 
