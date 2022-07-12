08d13e2b047356226720194d1564bd78bce63190
Vadim <65122882+Vadeem0550@users.noreply.github.com>
2022-07-12 23:18:01 +0300

    fix code

--- js/util.js
+++ js/util.js
@@ -1,10 +1,14 @@
-const getRandomValues = () => {
-  const max = (a, b) => a > b ? a : b;
-  const min = (a, b) => a < b ? a : b;
+const getRandomValues = (a, b) => {
+  const max = a > b ? a : b;
+  const min = a < b ? a : b;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getRandomArrEl = (el) => el[getRandomValues(0, el.length - 1)];
+const getRandomArrEl = (el) => {
+  if (el) {
+    return el[getRandomValues(0, el.length - 1)];
+  }
+};
 
 const getStringLength = (str, maxLength) => str.length <= maxLength;
 getStringLength('Some very long text', 40);
